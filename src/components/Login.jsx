11708739ddb92e57cc936dcf0b8703bcece97efe
Prashanth Dowlani <prashanth.work01@gmail.com>
2023-06-03 16:28:48 +0200

    login funciona con axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,97 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitLogin = async (data) => {
+    await axios
+      .post("http://localhost:5000/api/login", {
+        email: data.email,
+        password: data.password,
+      })
+      .then((response) => {
+        console.log(`Login Successful`);
+        localStorage.setItem(
+          "doctorsData",
+          JSON.stringify({
+            userId: response.data.userId,
+            token: response.data.token,
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(`Failed Communication ${error.data}`);
+      });
+  };
+
+  return (
+    <div className="contenedor-login">
+      <div className="formulario-login">
+        <h1> Iniciar Session :</h1>
+
+        <br />
+        <form className="form-login" onSubmit={handleSubmit(onSubmitLogin)}>
+          <label htmlFor="nombre">Email : </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="introduzca su e-mail"
+            {...register(
+              "email",
+              { required: true, message: "Requerido" },
+              {
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Ingresa un email válido",
+              }
+            )}
+          />
+          {errors.email && errors.email.type === "required" && (
+            <span> Plese enter a valid email</span>
+          )}
+          <br />
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="su contraseña"
+            {...register(
+              "password",
+              // {
+              //   pattern:
+              //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^])[A-Za-z\d\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^]{8,}$/,
+              //   message:
+              //     "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un caracter especial permitido: ! # @ $ % & / ( ) = ? * - + _ . : ; , ] [ { } ^",
+              // },
+              { required: true, message: "Requerido" }
+            )}
+          />
+          {errors.password && errors.password.type === "required" && (
+            <span> Required</span>
+          )}
+          {/* {errors.password && errors.password.type === "pattern" && (
+            <span>
+              La contraseña debe tener al menos 8 caracteres, incluyendo una
+              letra mayúscula, una letra minúscula, un número y un caracter
+              especial
+            </span>
+          )} */}
+
+          <br />
+          <button className="form-enviar" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,109 +4,144 @@ import { useForm } from "react-hook-form";
 const Signup = () => {
   const {
     register,
-    handelSubmit,
+    handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const formGestor = (info) => {
-    console.log(info);
-  };
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="signup-container">
-      <h1>Register Here </h1>
-      <br />
-      <div className="register">
-        <form onSubmit={formGestor}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Your name"
-            {...register("name", { minLength: 4, required: true })}
-          />
-          {errors.name && errors.name.type === "required" && <h4>Required</h4>}
-          {errors.name && errors.name.type === "minLength" && (
-            <h4>Must be at least 4 characters</h4>
-          )}
-          <br />
-          <label htmlFor="surname">Surname</label>
-          <input
-            type="text"
-            name="surname"
-            id="surname"
-            placeholder="Your surname"
-            {...register("surname", { minLength: 4, required: true })}
-          />
-          {errors.surname && errors.surname.type === "required" && (
-            <h4>Required</h4>
-          )}
-          {errors.surname && errors.surname.type === "minLength" && (
-            <h4>Must be at least 4 characters</h4>
-          )}
-          <br />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Your email"
-            {...register(
-              "email",
-              { required: true },
-              {
-                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Please enter a valid email",
-              }
+    <div className="contenedor-signup">
+      <div className="form-alta">
+        <h1>Formulario para Darse de Alta :</h1>
+        <br />
+        <div className="inputs">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="nombre">Nombre : </label>
+            <input
+              type="text"
+              name="nombre"
+              id="nombre"
+              placeholder="Nombre"
+              {...register("nombre", { minLength: 4, required: true })}
+            />
+            {errors.nombre && errors.nombre.type === "required" && (
+              <p>Campo Requerido</p>
             )}
-          />
-          {errors.email && errors.email.type === "required" && (
-            <h4>Required</h4>
-          )}
-          {errors.email && errors.email.type === "pattern" && (
-            <h4>Must be at least 4 characters</h4>
-          )}
-          <br />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Your password"
-            {...register(
-              "password",
-              { required: true },
-              {
-                pattern:
-                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^])[A-Za-z\d\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^]{8,}$/,
-                message:
-                  "Password must be at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one allowed special character",
-              }
+            {errors.nombre && errors.nombre.type === "minLength" && (
+              <p>Debe tener minimo 4 caracteres</p>
             )}
-          />
-          {errors.password && errors.password.type === "required" && (
-            <h4>Required</h4>
-          )}
-          {errors.password && errors.password.type === "pattern" && (
-            <h4>
-              MPassword must be at least 8 characters, including one uppercase
-              letter, one lowercase letter, one number, and one allowed special
-              character
-            </h4>
-          )}
-          <br />
-          <label htmlFor="rol">Select your Rol :</label>
-          <select name="rol" id="rol">
-            <option value="pacient">Pacient</option>
-            <option value="doctor">Doctor</option>
-            <option value="other">Other</option>
-          </select>
-          <br />
-          <div className="input-submit">
-            <input type="submit" value="submit" id="submit" />
-          </div>
-        </form>
+            <br />
+            <label htmlFor="apellidos">Apellidos : </label>
+            <input
+              type="text"
+              name="apellidos"
+              id="apellidos"
+              placeholder="apellidos"
+              {...register("apellidos", { minLength: 4, required: true })}
+            />
+            {errors.apellidos && errors.apellidos.type === "required" && (
+              <p className="fallo">Campo Requerido</p>
+            )}
+            {errors.apellidos && errors.apellidos.type === "minLength" && (
+              <p className="fallo">Debe tener minimo 4 caracteres</p>
+            )}
+            <br />
+            <label htmlFor="edad">Edad : </label>
+            <input
+              type="number"
+              name="edad"
+              id="edad"
+              placeholder="edad"
+              {...register("edad", { min: 18, required: true })}
+            />
+            {errors.edad && errors.edad.type === "required" && (
+              <p className="fallo">Campo Requerido</p>
+            )}
+            {errors.edad && errors.edad.type === "min" && (
+              <p className="fallo">Debe tener minimo 18 años</p>
+            )}
+            <br />
+            <label htmlFor="telef">Telefono : </label>
+            <input
+              type="tel"
+              name="telfono"
+              id="telefono"
+              placeholder="su telefono"
+              {...register("telefono", { required: true })}
+            />
+            {errors.telefono && errors.telefono.type === "required" && (
+              <p className="fallo">Campo Requerido</p>
+            )}
+            <br />
+            <label htmlFor="email">E-mail : </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="e-mail"
+              {...register(
+                "email",
+                { required: true },
+                {
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Ingresa un email válido",
+                }
+              )}
+            />
+            {errors.email && errors.email.type === "required" && (
+              <p className="fallo">Campo Requerido</p>
+            )}
+            {errors.email && errors.email.type === "password" && (
+              <p className="fallo">Debe tener un email válido</p>
+            )}
+            <br />
+            <label htmlFor="password">Contraseña: </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+              {...register(
+                "password",
+                {
+                  pattern:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^])[A-Za-z\d\!\#\@\$\%\&\/\(\)\=\?\*\-\+\-\_\.\:\;\,\]\[\{\}\^]{8,}$/,
+                  message:
+                    "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un caracter especial permitido: ! # @ $ % & / ( ) = ? * - + _ . : ; , ] [ { } ^",
+                },
+
+                { required: true }
+              )}
+            />
+            {errors.password && errors.password.type === "required" && (
+              <p className="fallo">Campo Requerido</p>
+            )}
+            {errors.password && errors.password.type === "pattern" && (
+              <p className="fallo">
+                La contraseña debe tener al menos 8 caracteres, incluyendo una
+                letra mayúscula, una letra minúscula, un número y un caracter
+                especial.
+              </p>
+            )}
+            <br />
+            <div>
+              <input
+                className="form-enviar"
+                type="submit"
+                value="Crear"
+                id="submit"
+              />
+            </div>
+          </form>
+        </div>
+        {/* <div className="alta">
+          <h2>
+            Ya tienes cuenta con nosotros
+            <Link to={"/login"} className="pulse">
+              Pulse aqui
+            </Link>
+          </h2>
+        </div> */}
       </div>
     </div>
   );
